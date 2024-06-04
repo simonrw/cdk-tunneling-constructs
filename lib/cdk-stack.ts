f6c2fcd48729fcb538a8522c00908d8e9eb52229
Simon Walker <simon.walker@localstack.cloud>
2024-06-04 16:27:18 +0100

    Add elasticache stack

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,11 +1,74 @@
-import { CfnOutput, CfnResource, RemovalPolicy, Stack, type StackProps } from "aws-cdk-lib";
+import { CfnOutput, CfnResource, RemovalPolicy, ResolutionTypeHint, Stack, type StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as neptune from '@aws-cdk/aws-neptune-alpha';
-import { IpAddresses, Vpc } from "aws-cdk-lib/aws-ec2";
+import { IpAddresses, SecurityGroup, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
 import PrivateTCPListener from "./constructs/private-tcp-listener";
+import { CfnCacheCluster, CfnSubnetGroup } from "aws-cdk-lib/aws-elasticache";
 
 
 export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+
+    // new NeptuneStack(this, "NeptuneStack");
+    new ElasticacheStack(this, "ElasticacheStack");
+  }
+};
+
+class ElasticacheStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const vpc = new Vpc(this, "VPC", {
+      ipAddresses: IpAddresses.cidr("10.0.16.0/24"),
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          name: "public",
+          subnetType: SubnetType.PUBLIC,
+          cidrMask: 26,
+        },
+        {
+          name: "private",
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 26,
+        },
+      ],
+    });
+
+    const securityGroup = new SecurityGroup(
+      this,
+      "RedisSecurityGroup",
+      {
+        securityGroupName: "redis-sec-group",
+        vpc,
+        allowAllOutbound: true,
+      });
+
+    const subnet = new CfnSubnetGroup(this, "SubnetGroup", {
+      subnetIds: vpc.privateSubnets.map(s => s.subnetId),
+      description: "Elasticache subnet group",
+    });
+
+    const db = new CfnCacheCluster(this, "CacheCluster", {
+      cacheSubnetGroupName: subnet.ref,
+      cacheNodeType: "cache.t2.micro",
+      engine: "redis",
+      numCacheNodes: 1,
+      vpcSecurityGroupIds: [securityGroup.securityGroupId],
+    });
+    db.applyRemovalPolicy(RemovalPolicy.DESTROY);
+
+    new PrivateTCPListener(this, "TCPListener", {
+      domainName: db.getAtt("RedisEndpoint.Address"),
+      port: db.getAtt("RedisEndpoint.Port", ResolutionTypeHint.NUMBER),
+      vpc,
+    });
+  }
+}
+
+class NeptuneStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
