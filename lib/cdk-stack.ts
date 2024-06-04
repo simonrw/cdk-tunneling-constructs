bec7ea888fc86c6088e2be68747dc93d0ca0ef91
Simon Walker <simon.walker@localstack.cloud>
2024-06-04 16:27:00 +0100

    PrivateTCPListener takes references

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -36,4 +36,4 @@ export class CdkStack extends Stack {
       value: cluster.clusterEndpoint.port.toString(),
     });
   }
-};
+}
--- lib/constructs/private-tcp-listener.ts
+++ lib/constructs/private-tcp-listener.ts
@@ -1,4 +1,4 @@
-import { CfnOutput, Duration } from "aws-cdk-lib";
+import { CfnOutput, Duration, Reference, Token } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import GetsIPAddress from "./gets-ip-address";
 import { NetworkLoadBalancer, Protocol } from "aws-cdk-lib/aws-elasticloadbalancingv2";
@@ -6,8 +6,8 @@ import { IpTarget } from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
 import { IVpc } from "aws-cdk-lib/aws-ec2";
 
 interface PrivateTCPListenerProps {
-  domainName: string,
-  port: number,
+  domainName: string | Reference,
+  port: number | Reference,
   vpc: IVpc,
   timeout?: Duration,
 }
@@ -19,22 +19,36 @@ export default class PrivateTCPListener extends Construct {
     super(scope, id);
 
     // get ip address of the neptune cluster
+    let domainName;
+    if (props.domainName instanceof Reference) {
+      domainName = Token.asString(props.domainName);
+    } else {
+      domainName = props.domainName;
+    }
+
     const ipAddressFetcher = new GetsIPAddress(this, "GetsIPAddress", {
-      domainName: props.domainName,
+      domainName,
     });
     const ipAddress = ipAddressFetcher.ipAddress();
-
-    const port = props.port;
 
     // add networking
     this.lb = new NetworkLoadBalancer(this, "LoadBalancer", {
       vpc: props.vpc,
       internetFacing: true,
     });
+
+    let port;
+    if (props.port instanceof Reference) {
+      port = Token.asNumber(props.port);
+    } else {
+      port = props.port;
+    }
+
     const listener = this.lb.addListener("Listener", {
       port,
       protocol: Protocol.TCP,
     });
+
     listener.addTargets("Target", {
       port,
       targets: [new IpTarget(ipAddress, port)],
